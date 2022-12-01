074ae0766743499b87bb3ffb85a02328b3f026a8
Benjamin Shu <bshu@bnl.gov>
2022-12-01 16:51:01 -0500

    Reduced number of predicates and tables used for edge/6 and path/6 relations.

--- predicates/graph.pl
+++ predicates/graph.pl
@@ -1,27 +1,16 @@
-:- table nuclide(_, _, _, po(<)).
 :- table level(_, _, _, po(<)).
-:- table decay(_, _, _, _, _, _, po(<)).
-:- table node(_, _, _, po(<)).
+:- table decay(_, _, _, _, _, _, _, _, po(<)).
 :- table edge(_, _, _, _, _, _, po(<)).
 :- table path(_, _, _, _, _, _, po(<)).
 
-node(Z, N, E) :-
-	nuclide(Z, N, _),
-	level(Z, N, E).
-
 edge(Z1, N1, E1, Z2, N2, E2) :-
-	node(Z1, N1, E1),
-	node(Z2, N2, E2),
+	level(Z1, N1, E1),
+	level(Z2, N2, E2),
 	decay(Z1, N1, E1, _, Z2, N2, E2, _).
 
 path(Z1, N1, E1, Z2, N2, E2) :-
-	node(Z1, N1, E1),
-	node(Z2, N2, E2),
 	edge(Z1, N1, E1, Z2, N2, E2).
 
-path(Z1, N1, E2, Z3, N3, E3) :-
-	node(Z1, N1, E1),
-	node(Z2, N2, E2),
-	node(Z3, N3, E3),
+path(Z1, N1, E1, Z3, N3, E3) :-
 	edge(Z1, N1, E1, Z2, N2, E2),
 	path(Z2, N2, E2, Z3, N3, E3).
