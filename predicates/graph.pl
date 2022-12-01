0a62d7a863831882c121feae2f21b0665a4b60a0
Benjamin Shu <bshu@bnl.gov>
2022-12-01 17:18:39 -0500

    Further reduced graph.pl predicates to only use decay relation.

--- predicates/graph.pl
+++ predicates/graph.pl
@@ -1,11 +1,8 @@
-:- table level(_, _, _, po(<)) as subsumptive.
 :- table decay(_, _, _, _, _, _, _, _, po(<)) as subsumptive.
 :- table edge(_, _, _, _, _, _, po(<)) as subsumptive.
 :- table path(_, _, _, _, _, _, po(<)) as subsumptive.
 
 edge(Z1, N1, E1, Z2, N2, E2) :-
-	level(Z1, N1, E1),
-	level(Z2, N2, E2),
 	decay(Z1, N1, E1, _, Z2, N2, E2, _).
 
 path(Z1, N1, E1, Z2, N2, E2) :-
