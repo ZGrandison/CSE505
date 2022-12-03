09dce92d2ffa5d5d8cd7ebc45bbfc3c61691da62
ZGrandison <73369239+ZGrandison@users.noreply.github.com>
2022-12-02 21:26:56 -0500

    Modified graph.pl to return path probability, added graph_test.pl to compare evaluation in online vs desktop swi-prolog

--- predicates/graph.pl
+++ predicates/graph.pl
@@ -1,13 +1,15 @@
-:- table decay(_, _, _, _, _, _, _, _, po(<)) as subsumptive.
-:- table edge(_, _, _, _, _, _, po(<)) as subsumptive.
-:- table path(_, _, _, _, _, _, po(<)) as subsumptive.
+:- table path/7.
 
-edge(Z1, N1, E1, Z2, N2, E2) :-
-	decay(Z1, N1, E1, _, Z2, N2, E2, _).
+/* This will give unexpected or wrong results when used */
+/*:- table path(_, _, _, _, _, _, po('<'/2)).*/ 
 
-path(Z1, N1, E1, Z2, N2, E2) :-
-	edge(Z1, N1, E1, Z2, N2, E2).
+edge(Z1, N1, E1, Z2, N2, E2, P) :-
+	decay(Z1, N1, E1, _, Z2, N2, E2, P1), P is P1 / 100.
 
-path(Z1, N1, E1, Z3, N3, E3) :-
-	edge(Z1, N1, E1, Z2, N2, E2),
-	path(Z2, N2, E2, Z3, N3, E3).
+path(Z1, N1, E1, Z2, N2, E2, P) :-
+	edge(Z1, N1, E1, Z2, N2, E2, P).
+
+path(Z1, N1, E1, Z3, N3, E3, P) :-
+	edge(Z1, N1, E1, Z2, N2, E2, P1),
+	path(Z2, N2, E2, Z3, N3, E3, P2),
+	P is P1 * P2.
