447186aec11add136ac192aaea6a51b9d2ef32b9
Benjamin Shu <bshu@bnl.gov>
2022-12-03 16:16:24 -0500

    Added fewer_steps/3 predicate for aggregating lists of decay codes.

--- predicates/graph.pl
+++ predicates/graph.pl
@@ -1,11 +1,16 @@
-:- table edge(_, _, _, _, _, _, po(>)) as subsumptive.
-:- table path(_, _, _, _, _, _, po(>)) as subsumptive.
+:- table edge(_, _, _, _, _, _, po(>), lattice(fewer_steps/3)) as subsumptive.
+:- table path(_, _, _, _, _, _, po(>), lattice(fewer_steps/3)) as subsumptive.
 
-edge(Z1, N1, E1, Z2, N2, E2, P, Step) :-
+fewer_steps(S1, S2, S) :-
+	length(S1, L1),
+	length(S2, L2),
+	(L1 < L2 -> S = S1 ; S = S2).
+
+edge(Z1, N1, E1, Z2, N2, E2, P, [Step]) :-
 	decay(Z1, N1, E1, Step, Z2, N2, E2, P).
 
-path(Z1, N1, E1, Z2, N2, E2, P, [Step]) :-
-	edge(Z1, N1, E1, Z2, N2, E2, P, Step).
+path(Z1, N1, E1, Z2, N2, E2, P, Steps) :-
+	edge(Z1, N1, E1, Z2, N2, E2, P, Steps).
 
 path(Z1, N1, E1, Z3, N3, E3, P, [Step|Steps]) :-
 	edge(Z1, N1, E1, Z2, N2, E2, P1, Step),
