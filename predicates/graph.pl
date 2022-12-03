d8937ce620b9fa993f9bd762b510526d3ccfa35b
Benjamin Shu <bshu@bnl.gov>
2022-12-02 22:02:10 -0500

    Renamed path/6 to path_exists/6.

--- predicates/graph.pl
+++ predicates/graph.pl
@@ -1,13 +1,13 @@
 :- table decay(_, _, _, _, _, _, _, _, po(<)) as subsumptive.
 :- table edge(_, _, _, _, _, _, po(<)) as subsumptive.
-:- table path(_, _, _, _, _, _, po(<)) as subsumptive.
+:- table path_exists(_, _, _, _, _, _, po(<)) as subsumptive.
 
 edge(Z1, N1, E1, Z2, N2, E2) :-
 	decay(Z1, N1, E1, _, Z2, N2, E2, _).
 
-path(Z1, N1, E1, Z2, N2, E2) :-
+path_exists(Z1, N1, E1, Z2, N2, E2) :-
 	edge(Z1, N1, E1, Z2, N2, E2).
 
-path(Z1, N1, E1, Z3, N3, E3) :-
+path_exists(Z1, N1, E1, Z3, N3, E3) :-
 	edge(Z1, N1, E1, Z2, N2, E2),
-	path(Z2, N2, E2, Z3, N3, E3).
+	path_exists(Z2, N2, E2, Z3, N3, E3).
