755d1f4c5ae015d742eb69f40570e2db3f5c3b9f
Benjamin Shu <bshu@bnl.gov>
2022-12-05 22:07:22 -0500

    Correcting typos in 'aggregated' queries.

--- predicates/graph.pl
+++ predicates/graph.pl
@@ -54,13 +54,13 @@ fewer_steps(S1, S2, S) :-
 	length(S2, L2),
 	(L1 < L2 -> S = S1 ; S = S2).
 
-edge_with_table(Z1, N1, E1, Z2, N2, E2, P, Step) :-
+edge_aggregated(Z1, N1, E1, Z2, N2, E2, P, Step) :-
 	decay(Z1, N1, E1, Step, Z2, N2, E2, P).
 
-path_with_table(Z1, N1, E1, Z2, N2, E2, P, [Step]) :-
+path_aggregated(Z1, N1, E1, Z2, N2, E2, P, [Step]) :-
 	edge_aggregated(Z1, N1, E1, Z2, N2, E2, P, Step).
 
-path_with_table(Z1, N1, E1, Z3, N3, E3, P, [Step|Steps]) :-
+path_aggregated(Z1, N1, E1, Z3, N3, E3, P, [Step|Steps]) :-
 	edge_aggregated(Z1, N1, E1, Z2, N2, E2, P1, Step),
 	path_aggregated(Z2, N2, E2, Z3, N3, E3, P2, Steps),
 	P is (P1 * P2).
